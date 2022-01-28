22b990cb0be8fb778791c7cdc3f3b768f13baa7e
EduardoAraujo047 <eduardoaraujo2609@gmail.com>
2022-01-28 15:58:00 -0300

    Projeto ReactJS para consulta de CEP

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+import GlobalStyle  from './styles/global'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Dashboard/>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+
+export default App;
