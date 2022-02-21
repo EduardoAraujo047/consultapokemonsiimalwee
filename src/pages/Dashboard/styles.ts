2d2f34355216045a0ef0886cf5b3975ccd345cd6
EduardoAraujo047 <eduardoaraujo2609@gmail.com>
2022-02-20 21:12:08 -0300

    Estilização da mensagem de erro

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -21,41 +21,67 @@ export const Background = styled.div`
 
 export const Form = styled.form`
     margin-top: 20px;
-    max-width: 800px;
+    max-width: 700px;
 
     display: flex;
 
     input{
         flex: 1;
+        font-family: "JetBrains Mono", monospace;
+        font-weight: bold;
         height: 60px;
         padding: 0 25px;
         border: 2px solid #000;
-        border-radius: 5px 0 0 5px;
+        border-radius: 5px 5px 5px 5px;
         color: #3a3a3a;
-        border-right: 0;
         transition: background-color 0.2s;
-
-        &:focus {
-            background: #ebebeb;
-        }
-
     }
 
     button {
-        width: 210px;
-        height: 60px;
-        background: #FFCC00;
-        border-radius: 0 5px 5px 0;
-        border-top: 2px solid #000;
-        border-right: 2px solid #000;
-        border-bottom: 2px solid #000;
-        border-left: 0;
-        color: #fff;
+        margin-left: 10px;
+        align-items: center;
+        appearance: none;
+        background-color: #f6eb49;
+        border-radius: 4px;
+        border-width: 0;
+        box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#c5be53 0 -3px 0 inset;
+        box-sizing: border-box;
+        color: #36395A;
+        cursor: pointer;
+        display: inline-flex;
+        font-family: "JetBrains Mono", monospace;
         font-weight: bold;
-        transition: background-color 0.2s;
+        width: 150px;
+        height: 58px;
+        justify-content: center;
+        line-height: 1;
+        list-style: none;
+        overflow: hidden;
+        padding-left: 16px;
+        padding-right: 16px;
+        position: relative;
+        text-align: left;
+        text-decoration: none;
+        transition: box-shadow .15s,transform .15s;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        white-space: nowrap;
+        will-change: box-shadow,transform;
+        font-size: 18px;
+
+        &:focus {
+            box-shadow: #c5be53 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #c5be53 0 -3px 0 inset;
+        }
 
         &:hover {
-            background: ${shade(0.2, "#D5A100")}
+            box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #c5be53 0 -3px 0 inset;
+            transform: translateY(-2px);
+        }
+
+        &:active {
+            box-shadow: #c5be53 0 3px 7px inset;
+            transform: translateY(2px);
         }
     }
 `;
@@ -66,7 +92,6 @@ export const Pokemons = styled.div`
     max-width: 795px;
     display: flex;
     
-    flex: 1; 
     flex-direction: row;
     flex-wrap: wrap;
     
@@ -76,14 +101,12 @@ export const Pokemons = styled.div`
         height: 300px;
         margin-right: 15px;
         margin-bottom: 15px;
-        // background-color: #fff;
         border-radius: 5px 5px 5px 5px;
         text-decoration: none;
-
         align-items: center;
-
+        
         transition: transform 0.2s;
-
+    
         &:hover {
             transform: translate(2px);
         }
@@ -137,4 +160,14 @@ export const Pokedex = styled.p`
 
         margin-left: 81%;
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    font-family: "JetBrains Mono", monospace;
+    font-style: italic;
+    font-weight: bold;
+    margin-top: 5px;
+    margin-left: 10px;
 `;
