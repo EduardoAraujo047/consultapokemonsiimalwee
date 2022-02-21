b5655003e0496fcd0cec522bebded6654bc11911
EduardoAraujo047 <eduardoaraujo2609@gmail.com>
2022-02-20 21:11:39 -0300

    Configuração das mensagens de erro

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
-import { Container, Form, Background, Pokemons, Descricao, Pokedex } from "./styles";
+import { Container, Form, Background, Pokemons, Descricao, Pokedex, Error } from "./styles";
 import api from '../../services/api';
 import bgPoke from '../../images/p.png';
 import pokebola from '../../images/pokebola.png';
+import splash from '../../images/splash.png';
 import { useState, FormEvent } from "react";
 
 interface PokeProps {
@@ -47,20 +48,29 @@ interface PokeProps {
 
 const Dashboard: React.FC = () => {   
     const [newPoke, setNewPoke] = useState('');
+    const [inputError, setInputError] = useState('');
     const [pokes, setPoke] = useState<PokeProps[]>([]);
-
+    
+    
     const pesquisarPoke = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        
+        if(!newPoke){
+            setInputError("Digite um pokémon para pesquisar. (Ex: Pikachu)")
+            return;
+        }
 
         try {
             const response = await api.get(`${newPoke}`);
             const pokeDados = response.data;
 
             setPoke([...pokes, pokeDados])
+            setNewPoke('');
+            setInputError('');
 
             console.log(pokes);
         } catch(err){
-
+            setInputError("Pokémon não encontrado ou inexistente.");
         }
     };
 
@@ -70,29 +80,31 @@ const Dashboard: React.FC = () => {
             <Background>
                 <img src={bgPoke} />
                 <Form onSubmit={pesquisarPoke}>
-                    <input type="text" placeholder="Busque por um Pokémon" onChange={e => setNewPoke(e.target.value)} />
-                    <button type="submit">Buscar</button>    
+                    <input type="text" placeholder="Busque por um Pokémon" value={newPoke} onChange={e => setNewPoke(e.target.value)} />
+                    <button type="submit">Buscar</button>
                 </Form>
             </Background>
 
-                <Pokemons>
-                    {pokes.map(poke => (
-                        <a href="#" className={poke.types[0].type.name}>
-                            <Pokedex>
-                                <p>#{poke.id}</p>
-                                <img src={pokebola}/>
-                            </Pokedex>
-                            <img src={poke.sprites.front_default} />
-                            <Descricao>
-                                <strong>{poke.forms[0].name.toUpperCase()}</strong>
-                                <p>{poke.types[0].type.name.charAt(0).toUpperCase() + poke.types[0].type.name.slice(1)}</p>
-                                <p>{poke.abilities[1].ability.name.charAt(0).toUpperCase() + poke.abilities[1].ability.name.slice(1)}</p>
-                            </Descricao>
-                        </a>
-                    ))}
-                </Pokemons>
-                <hr />
-                <h6>Projeto API Malwee - Dev: Eduardo Ronchi de Araújo ©</h6>
+            {inputError && <Error>{inputError}</Error>}
+
+            <Pokemons>
+                {pokes.map(poke => (
+                    <a href="#" className={poke.types[0].type.name}>
+                        <Pokedex>
+                            <p>#{poke.id}</p>
+                            <img src={pokebola}/>
+                        </Pokedex>
+                        <img src={poke.sprites.front_default} />
+                        <Descricao>
+                            <strong>{poke.forms[0].name.toUpperCase()}</strong>
+                            <p>{poke.types[0].type.name.charAt(0).toUpperCase() + poke.types[0].type.name.slice(1)}</p>
+                            <p>{poke.abilities[1].ability.name.charAt(0).toUpperCase() + poke.abilities[1].ability.name.slice(1)}</p>
+                        </Descricao>
+                    </a>
+                ))}
+            </Pokemons>
+            <hr />
+            <h6>Projeto API Malwee - Dev: Eduardo Ronchi de Araújo ©</h6>
         </Container>  
     );
 }
